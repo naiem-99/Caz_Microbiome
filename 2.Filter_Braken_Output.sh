51a9b0b433dc2679de3d504acbb2c6a63922b054
Md Naiem Hossain <naiem2217@gmail.com>
2026-02-14 12:16:02 +0600

    Update 2.Filter_Braken_Output.sh

--- 2.Filter_Braken_Output.sh
+++ 2.Filter_Braken_Output.sh
@@ -5,37 +5,42 @@ module avail python
 module load PaM/environment
 module load python-3.9.18
 #--------------------------------------------------------------
-#https://github.com/jenniferlu717/KrakenTools/tree/master
-#make lsf file as a bash script to run KrakenTools filter_bracken.out.py for filter Homo Sapiens (--exclude 9606)
-#-----------------------------------------------------------------------------------------------
+#before running the script 'run dos2unix braken_filtered_v2.sh
+#then run as bash braken_filtered_v2.sh
+#-------------------------------------------------------------------------
 #!/bin/bash
-#BSUB -G team216f
-#BSUB -q normal
-#BSUB -M 8000
-#BSUB -R "select[mem=8000] rusage[mem=8000]"
-#BSUB -J filter_bracken
-#BSUB -o filter_bracken.%J.out
-#BSUB -e filter_bracken.%J.err
-
+# ===============================
+# LOAD MODULE (if needed)
+# ===============================
 module load python-3.9.18
 
+# ===============================
+# PATHS
+# ===============================
 IN_DIR="/data/pam/team216/ma32/scratch/metagenome/caz/bracken_output"
 OUT_DIR="/data/pam/team216/ma32/scratch/metagenome/caz/filtered_bracken"
+SCRIPT="/data/pam/team216/ma32/scratch/metagenome/caz/krakentools_output/KrakenTools/filter_bracken.out.py"
 
+# ===============================
+# CREATE OUTPUT DIRECTORY
+# ===============================
 mkdir -p "$OUT_DIR"
+
+# ===============================
+# RUN FILTER
+# ===============================
+echo "Starting Bracken filtering..."
 
 for f in "$IN_DIR"/*.bracken; do
   base=$(basename "$f" .bracken)
 
-  python3 /data/pam/team216/ma32/scratch/metagenome/caz/krakentools_output/KrakenTools/filter_bracken.out.py \
+  echo "Processing $base ..."
+
+  python3 "$SCRIPT" \
     -i "$f" \
     -o "$OUT_DIR/${base}.filtered.bracken" \
     --exclude 9606
 done
-#-----------------------------------------------------------------------------------------------------------------------------
-#---------------------- make the below script as filter_braken.lsf then upload in HPC , and then run as bsub < filtered_bracken.lsf
-#check the job status by running : bjobs, the you will be shown the status whether pending or completion
-#if you waana kill a job just type bkill jobID
 
-
+echo "Filtering completed!"
 #--------------------------------------------------------------
