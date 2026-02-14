b4847e351c4ff70cd8db200ac6e2af80504811c3
Md Naiem Hossain <naiem2217@gmail.com>
2026-02-14 14:14:49 +0600

    Update and rename 4.1-Python-Script to 4.1.alphaDiversity.sh

--- 4.Diversity_Calculation.py/4.1.alphaDiversity.sh
+++ 4.Diversity_Calculation.py/4.1.alphaDiversity.sh
@@ -2,6 +2,16 @@
 
 #-------------------------From Python Script------------------------------------
 #----------------------------------alpha_diversity------------------------------
+wget https://raw.githubusercontent.com/jenniferlu717/KrakenTools/master/DiversityTools/alpha_diversity.py
+
+#run the script from here ma32@farm22-head2:/data/pam/team216/ma32/scratch/metagenome/caz/alphaDiversity$
+
+
+
+
+
+
+
 
 
 
@@ -9,8 +19,4 @@
 
 
 
-#----------------------------------Beta_diversity-------------------------------
 
-
-
-#-------------------------------------------------------------------------------
