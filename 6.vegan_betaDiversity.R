95bf1438967eb667ec673c42b118fa7f78776bc6
Md Naiem Hossain <naiem2217@gmail.com>
2026-03-03 17:31:01 +0600

    Update 6.vegan_betaDiversity.R

--- 6.vegan_betaDiversity.R
+++ 6.vegan_betaDiversity.R
@@ -1,5 +1,21 @@
 
 #---------------------------------------Beta------------------------------------------------------------
+# ===============================
+library(dplyr)
+library(tidyr)
+Merge_Sample_Species <- read.csv("Merge_Sample_Species.csv", check.names = FALSE)
+dim(Merge_Sample_Species)
+head(Merge_Sample_Species)
+#-------------------------------------------------------------------------------------------------------
+#https://rpubs.com/mrgambero/taxa_alpha_beta
+rm(list = setdiff(ls(), "Merge_Sample_Species")); gc()
+Metadata_All <-Merge_Sample_Species%>%select(SampleID, TP, Type, Treatmentcode)
+#---------------------------------------------------------------------------------------------------------
+species_matrix <- Merge_Sample_Species %>%select(-TP, -Type, -Treatmentcode) %>%column_to_rownames("SampleID") %>%data.matrix()
+# ---------------------------------------------------------------------------------------------------------
+
+
+
 
 
 #-----------------------------------------------------------------------------------------------------------
